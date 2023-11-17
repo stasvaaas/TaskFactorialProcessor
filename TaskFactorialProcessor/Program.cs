49f927f093d01545a365d98ada8c6b8b78e72f15
stasvaaas <105922844+stasvaaas@users.noreply.github.com>
2023-11-17 12:12:37 +0200

    Add project files.

---
+++ TaskFactorialProcessor/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskFactorialProcessor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskFactorialProcessor processor1 = new TaskFactorialProcessor();
+            Console.WriteLine("Parallel Mode:");
+            processor1.Go(10, true);
+
+            Thread.Sleep(2000);
+
+            TaskFactorialProcessor processor2 = new TaskFactorialProcessor();
+            Console.WriteLine("\nSequential Mode:");
+            processor2.Go(10, false);
+        }
+    }
+}
---
+++ TaskFactorialProcessor/TaskFactorialProcessor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFactorialProcessor
+{
+    internal class TaskFactorialProcessor
+    {
+            private Stopwatch _stopwatch;
+            private int _maxValue;
+            private int _counter;
+            public TaskFactorialProcessor()
+            {
+                _stopwatch = new Stopwatch();
+            }
+            public void Go(int param, bool parallelMode)
+            {
+                _maxValue = param;
+                if (parallelMode)
+                {
+                    StartFactorialParallel(param);
+                }
+                else
+                {
+                    StartFactorialSequential(param);
+                }
+            }
+            void StartFactorialSequential(int param)
+            {
+                _stopwatch.Start();
+                for (int i = 1; i <= param; i++)
+                {
+                    PrintFactorial(i);
+                }
+            }
+
+            void StartFactorialParallel(int param)
+            {
+                _stopwatch.Start();
+                for (int i = 1; i <= param; i++)
+                {
+                int n = i;
+                new Task(PrintFactorial,n).Start();
+                }
+            }
+
+            private void PrintFactorial(object? n)
+            {
+                int param = (int)n;
+                int result = Factorial(param);
+                Console.WriteLine($"fact of {param} is {result}");
+                if (++_counter == _maxValue)
+                {
+                    _stopwatch.Stop();
+                    Console.WriteLine($"ms {_stopwatch.ElapsedMilliseconds} ticks {_stopwatch.ElapsedTicks}");
+                }
+            }
+            private static int Factorial(int n)
+            {
+                if (n == 1 || n == 0) return 1;
+                return n * Factorial(n - 1);
+            }
+    }
+}
